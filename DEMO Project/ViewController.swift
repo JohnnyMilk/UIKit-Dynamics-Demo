9e67fa03ce060b2d9142ae5c8fb8bdae098b4eee
Johnny Wang <johnny@doubleservice.com>
2018-05-14 14:58:44 +0800

    [add] first commit
    # done

--- DEMO Project/ViewController.swift
+++ DEMO Project/ViewController.swift
@@ -8,18 +8,89 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum itemType: String {
+    case like = "like"
+    case love = "love"
+    case haha = "haha"
+    case ouch = "ouch"
+}
 
+class ViewController: UIViewController, UICollisionBehaviorDelegate {
+    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var countLabel: UILabel!
+    var animator: UIDynamicAnimator?
+    var gravity = UIGravityBehavior()
+    var collision = UICollisionBehavior()
+    var behavior = UIDynamicItemBehavior()
+    var collisionCount = 0
+    
+    @IBAction func onLikeItemClicked(_ sender: Any) {
+        addBehaviour(item: .like)
+    }
+    
+    @IBAction func onLoveItemClicked(_ sender: Any) {
+        addBehaviour(item: .love)
+    }
+    
+    @IBAction func onHahaItemClicked(_ sender: Any) {
+        addBehaviour(item: .haha)
+    }
+    
+    @IBAction func onOuchItemClicked(_ sender: Any) {
+        addBehaviour(item: .ouch)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setBackgroundImage()
+        setDynamicBehavior()
+    }
+   
+    private func setBackgroundImage() {
+        view.backgroundColor = UIColor.init(patternImage: #imageLiteral(resourceName: "wallpaper"))
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func setDynamicBehavior() {
+        animator = UIDynamicAnimator(referenceView: contentView)
+        
+        animator?.addBehavior(gravity)
+        
+        collision.translatesReferenceBoundsIntoBoundary = true
+        collision.collisionDelegate = self
+        animator?.addBehavior(collision)
+        
+        behavior = UIDynamicItemBehavior()
+        behavior.elasticity = 0.7
+        behavior.friction = 0.3
+        behavior.resistance = 0.1
+        behavior.allowsRotation = true
+        animator?.addBehavior(behavior)
     }
-
-
+    
+    private func addBehaviour(item: itemType) {
+        let size = CGSize(width: 60.0, height: 60.0)
+        let startPoint = CGPoint(x: 65.0, y: 65.0)
+        
+        let imageView = UIImageView(image: UIImage(named: item.rawValue))
+        imageView.frame = CGRect(origin: startPoint, size: size)
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = size.width / 2.0
+        contentView.addSubview(imageView)
+        
+        gravity.addItem(imageView)
+        collision.addItem(imageView)
+        behavior.addItem(imageView)
+        
+        let pushBehaviour = UIPushBehavior(items: [imageView], mode: .instantaneous)
+        pushBehaviour.pushDirection = CGVector(dx: 2.5, dy: 1.5)
+        animator?.addBehavior(pushBehaviour)
+    }
+    
+    
+    func collisionBehavior(_ behavior: UICollisionBehavior, endedContactFor item1: UIDynamicItem, with item2: UIDynamicItem) {
+        collisionCount += 1
+        countLabel.text = String(collisionCount)
+    }
 }
 
